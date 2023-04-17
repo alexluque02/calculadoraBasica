c119bf2f59002c1a76e6e0345682d7a24f8b7356
Alexander Luque Hoffrogge <alexanderluquehoffrogge@gmail.com>
2023-04-17 09:23:00 +0200

    Suma hecha

--- src/ejercicio/Principal.java
+++ src/ejercicio/Principal.java
@@ -18,6 +18,15 @@ public class Principal {
 					+ "0. Salir");
 			menu=Integer.parseInt(sc.nextLine());
 			
+			switch(menu) {
+			case 1:
+				System.out.println("Diga un número");
+				n1=Double.parseDouble(sc.nextLine());
+				System.out.println("Diga otro número");
+				n2=Double.parseDouble(sc.nextLine());
+				System.out.println("La suma es: "+(n1+n2));
+				break;
+			}
 		}while(menu!=0);
 	}
 }
